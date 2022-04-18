2b258934c4d46e397c40c1050d5a3d2b63ca1a17
Vladislav Sukhov <vladislav.sukhov.d@gmail.com>
2022-04-18 13:36:34 -0500

    Initial Commit

---
+++ DoughRise/DoughRiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoughRiseApp.swift
+//  DoughRise
+//
+//  Created by Mark on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct DoughRiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
