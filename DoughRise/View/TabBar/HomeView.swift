89f689241ddc2d3aa7a683f1e7e86eade28e1555
Vladislav Sukhov <vladislav.sukhov.d@gmail.com>
2022-04-18 21:39:19 -0500

    [EDIT] refactoring

--- DoughRise/View/TabBar/HomeView.swift
+++ DoughRise/View/TabBar/HomeView.swift
@@ -10,66 +10,66 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         ScrollView {
-        VStack(spacing: 16){
-            //MARK: - NavBar
-            HStack(alignment: .center, spacing: 0){
-                Spacer()
-                HStack{
-                Text("SEPTEMBER 2021")
-                    .foregroundColor(Color(AppPalette.shared.mainTextColor))
-                    .font((Font(AppFontBook.shared.titleSemibold)))
-            Image("back-arrow")
-                }
-                Spacer()
-            Image("plus")
-                
-            }
-            .padding(.top, 24)
-            .padding(.horizontal, 29)
-            
-            Text("$0 SPENT OF $6,000")
-                .foregroundColor(Color(AppPalette.shared.mainTextColor))
-                .font((Font(AppFontBook.shared.largeTitle)))
-            
-            VStack{
-                GradientProgressView(
-                    cornerRadius: 10,
-                    height: 35,
-                    gradient: Gradient(colors: [Color(AppPalette.shared.budgetGradientStart), Color(AppPalette.shared.budgetGradientEnd)])
-                )
-                .padding(.horizontal, 19)
-                
-                HStack{
-                    Text("$6,000 Left")
-                        .foregroundColor(Color(AppPalette.shared.mainTextColor))
-                        .font((Font(AppFontBook.shared.subheadSemibold)))
+            VStack(spacing: 16){
+                //MARK: - NavBar
+                HStack(alignment: .center, spacing: 0){
                     Spacer()
-                    Text("$1,000 Spent")
+                    HStack(spacing: 0){
+                        Text("SEPTEMBER 2021")
+                            .foregroundColor(Color(AppPalette.shared.mainTextColor))
+                            .font((Font(AppFontBook.shared.titleSemibold)))
+                        Image("back-arrow")
+                    }
+                    Spacer()
+                    Image("plus")
+                    
+                }
+                .padding(.top, 24)
+                .padding(.horizontal, 29)
+                
+                Text("$0 SPENT OF $6,000")
+                    .foregroundColor(Color(AppPalette.shared.mainTextColor))
+                    .font((Font(AppFontBook.shared.largeTitle)))
+                
+                VStack{
+                    GradientProgressView(
+                        cornerRadius: 10,
+                        height: 35,
+                        gradient: Gradient(colors: [Color(AppPalette.shared.budgetGradientStart), Color(AppPalette.shared.budgetGradientEnd)])
+                    )
+                    .padding(.horizontal, 19)
+                    
+                    HStack{
+                        Text("$6,000 Left")
+                            .foregroundColor(Color(AppPalette.shared.mainTextColor))
+                            .font((Font(AppFontBook.shared.subheadSemibold)))
+                        Spacer()
+                        Text("$1,000 Spent")
+                            .foregroundColor(Color(AppPalette.shared.mainTextColor))
+                            .font((Font(AppFontBook.shared.subheadSemibold)))
+                    }.padding(.horizontal, 25)
+                }
+                
+                
+                VStack(spacing: 5){
+                    Text("Breakdown of your Budget").kerning(1.12)
                         .foregroundColor(Color(AppPalette.shared.mainTextColor))
-                        .font((Font(AppFontBook.shared.subheadSemibold)))
-                }.padding(.horizontal, 25)
-            }
-            
-       
-            VStack(spacing: 5){
-                Text("Breakdown of your Budget").kerning(1.12)
-                .foregroundColor(Color(AppPalette.shared.mainTextColor))
-                .font((Font(AppFontBook.shared.titleMedium)))
-            
-            
-            HStack{
-            Text("Sort: Alphabetical Order").kerning(1.12)
-                .foregroundColor(Color(AppPalette.shared.mainTextColor))
-                .font((Font(AppFontBook.shared.headlineMedium)))
-                .padding(.leading, 27)
-                Spacer()
-            }
+                        .font((Font(AppFontBook.shared.titleMedium)))
+                    
+                    
+                    HStack{
+                        Text("Sort: Alphabetical Order").kerning(1.12)
+                            .foregroundColor(Color(AppPalette.shared.mainTextColor))
+                            .font((Font(AppFontBook.shared.headlineMedium)))
+                            .padding(.leading, 27)
+                        Spacer()
+                    }
+                    
+                }
+                BudgetsView().padding(.horizontal, 6)
                 
             }
-            BudgetsView().padding(.horizontal, 6)
-                
-        }
-       
+            
         }}
 }
 
